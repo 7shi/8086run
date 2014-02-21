07b0169d82d1fa714b15689824f3c7529f69519d
7shi <7shi@live.jp>
2014-02-21 11:29:50 +0900

    デバッグ用にメモリをダンプする関数を追加: dump()

--- main.cpp
+++ main.cpp
@@ -129,6 +129,19 @@ debug(FILE *f) {
             *ES, *SS, *DS, *CS, IP, CS[IP], CS[IP + 1], CS[IP + 2]);
 }
 
+void dump(FILE *f, SReg *seg, uint16_t addr, int len) {
+    fprintf(f, "org 0x%04x ; %04x:%04x\n", addr, seg->v, addr);
+    for (int i = 0; i < len; ++i) {
+        if (i & 7) {
+            fprintf(f, ", ");
+        } else {
+            if (i > 0)fprintf(f, "\n");
+            fprintf(f, "db ");
+        }
+        fprintf(f, "0x%02x", (*seg)[addr + i]);
+    }
+}
+
 inline uint16_t read16(uint8_t *p) {
     return p[0] | (p[1] << 8);
 }

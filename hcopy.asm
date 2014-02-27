46566b7ef5936743700808bf6f2d40ca47977425
7shi <7shi@live.jp>
2014-02-27 18:08:57 +0900

    hcopyコマンドに/sオプションで送信を実装 * BIOS側の対応は後回しでコマンドのみ * nearジャンプは未対応のためshortを指定

--- hcopy.asm
+++ hcopy.asm
@@ -3,57 +3,109 @@
 
 org 0x100
 
-%define fname end
-%define buf end + 0x100
+%define argc 0x80
+%define argv 0x81
+%define fname bss
+%define buf bss + 0x100
 
 start:
-	; fname = args
 	mov ch, 0
-	mov cl, [0x80]
+	mov cl, [argc]
 	sub cx, 1
-	jbe error_usage
-	mov si, 0x81
+	jnbe short start0
+	jmp error_usage
+start0:
+	mov si, argv
 	cmp [si], byte " "
-	jnz copy
+	jnz short checkp
 	inc si
-copy:
+
+checkp:
+	; check /s
+	xor dx, dx
+	cmp [si], word "/s"
+	jnz short copyarg
+	cmp [si+2], byte " "
+	jnz short copyarg
+	add si, 3
+	sub cx, 3
+	jnbe short checkp0
+	jmp error_usage
+checkp0:
+	mov dx, 1
+
+copyarg:
+	; fname = args
 	mov di, fname
 	cld
 	rep movsb
 	mov [di], byte 0
+	test dx, dx
+	jnz short send
 
 	; hopen fname
-	mov ah, 0xfe
+	mov ax, 0xfe00
 	mov dx, fname
 	int 0x13
-	jc error_open
+	jc short error_hopen
 
 	; bx = create fname
 	mov ah, 0x3c
 	xor cx, cx
 	mov dx, fname
 	int 0x21
-	jc error_create
+	jc short error_create
 	mov bx, ax
 
-hcopy:
-	; hcopy fname
+hread:
+	; hread buf 512
 	mov ah, 0xff
 	mov dx, buf
 	mov cx, 512
 	int 0x13
 	test cx, cx
-	jz close
+	jz short close
 
+write:
 	; write bx buf 512
-loop:
 	mov ah, 0x40
 	mov dx, buf
 	int 0x21
-	jc error_write
+	jc short error_write
 	sub cx, ax
-	jnbe loop
-	jmp hcopy
+	jnbe short write
+	jmp hread
+
+send:
+	; bx = open fname
+	mov ax, 0x3d00
+	mov dx, fname
+	int 0x21
+	jc short error_open
+	mov bx, ax
+
+	; hcreate fname
+	mov ax, 0xfe01
+	mov dx, fname
+	int 0x13
+	jc short error_hcreate
+
+read:
+	; read bx buf 512
+	mov ah, 0x3f
+	mov dx, buf
+	mov cx, 512
+	int 0x21
+	jc short error_read
+	test ax, ax
+	jz short close
+	
+	; hwrite buf cx
+	mov cx, ax
+	mov ah, 0xff
+	mov dx, buf
+	int 0x13
+	jmp read
 
 close:
 	; close bx
@@ -63,6 +115,7 @@ close:
 	; exit 0
 	mov ax, 0x4c00
 	int 0x21
+	jmp close
 
 error_usage:
 	mov ah, 9
@@ -70,12 +123,24 @@ error_usage:
 	int 0x21
 	jmp exit1
 
+error_hopen:
+	mov dx, err_hopen
+	jmp showfnexit
+
+error_hcreate:
+	mov dx, err_hcreate
+	jmp showfnexit
+
 error_open:
 	mov dx, err_open
 	jmp showfnexit
 
 error_create:
 	mov dx, err_create
+	jmp showfnexit
+
+error_read:
+	mov dx, err_read
 	jmp showfnexit
 	
 error_write:
@@ -91,9 +156,14 @@ exit1:
 	mov ax, 0x4c01
 	int 0x21
 
-err_usage db "usage: hcopy filename", 13, 10, "$"
-err_open db "can not open in host: $"
-err_create db "can not create: $"
-err_write db "can not write: $"
+err_usage   db "usage: hcopy [/s] filename", 10
+            db "  default  copy from host", 10
+            db "  /s       send to host", 10, "$"
+err_hopen   db "can not open in host: $"
+err_hcreate db "can not create in host: $"
+err_open    db "can not open: $"
+err_create  db "can not create: $"
+err_read    db "can not read: $"
+err_write   db "can not write: $"
 
-end:
+bss:

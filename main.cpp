256ef375c77e6eafdc7003d3e5797f5fc2fb60d8
7shi <7shi@live.jp>
2014-03-07 00:17:02 +0900

    shift(): 無駄な代入を抑制

--- main.cpp
+++ main.cpp
@@ -731,21 +731,24 @@ void intr(int n) {
 }
 
 inline void shift(Operand *opr, int c, uint8_t *p) {
-    int val = opr->u(), m = opr->w ? 0x8000 : 0x80;
+    int val, m = opr->w ? 0x8000 : 0x80;
     switch ((p[1] >> 3) & 7) {
         case 0: // rol
+            val = opr->u();
             for (int i = 0; i < c; ++i)
                 val = (val << 1) | (CF = val & m);
             OF = CF ^ bool(val & m);
             *opr = val;
             break;
         case 1: // ror
+            val = opr->u();
             for (int i = 0; i < c; ++i)
                 val = (val >> 1) | ((CF = val & 1) ? m : 0);
             OF = CF ^ bool(val & (m >> 1));
             *opr = val;
             break;
         case 2: // rcl
+            val = opr->u();
             for (int i = 0; i < c; ++i) {
                 val = (val << 1) | CF;
                 CF = val & (m << 1);
@@ -754,6 +757,7 @@ inline void shift(Operand *opr, int c, uint8_t *p) {
             *opr = val;
             break;
         case 3: // rcr
+            val = opr->u();
             for (int i = 0; i < c; ++i) {
                 bool f1 = val & 1, f2 = val & m;
                 val = (val >> 1) | (CF ? m : 0);
@@ -764,7 +768,7 @@ inline void shift(Operand *opr, int c, uint8_t *p) {
             break;
         case 4: // shl/sal
             if (c > 0) {
-                val <<= c;
+                val = opr->u() << c;
                 *opr = opr->setf(val);
                 CF = val & (m << 1);
                 OF = CF != bool(val & m);
@@ -772,7 +776,7 @@ inline void shift(Operand *opr, int c, uint8_t *p) {
             break;
         case 5: // shr
             if (c > 0) {
-                val >>= c - 1;
+                val = opr->u() >> (c - 1);
                 *opr = opr->setf(val >> 1);
                 CF = val & 1;
                 OF = val & m;
